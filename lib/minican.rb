e26746d131eaae47c08636921833fd1072e23b29
Eloy Gomez <eloy@indeos.es>
2015-12-09 14:09:30 +0100

    Initial commit

---
+++ lib/minican.rb
@@ -0,0 +1,5 @@
+require "minican/version"
+
+module Minican
+  # Your code goes here...
+end
---
+++ spec/minican_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Minican do
+  it 'has a version number' do
+    expect(Minican::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
